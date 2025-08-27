47e9dce89a3236ef89ab4d7c9380efac0219afbc
Zhu Liang <zhuliangg11@gmail.com>
2025-08-27 23:03:16 +0800

    feat: add deepseek v3.1

---
+++ projects/typescript-narrowing/wrong/deepseek-v3.1-2-2.ts
@@ -0,0 +1,38 @@
+type ModelInput = {
+  inputTokenLimit: number;
+  name: "gpt-4";
+};
+
+type ModelOutput = {
+  outputTokenLimit: number;
+  name: "gpt-3.5-turbo";
+};
+
+type ModelOther = {
+  name: string;
+};
+
+type Model = ModelInput | ModelOutput | ModelOther;
+
+function getModelInfo(model: Model) {
+  switch (model.name) {
+    case "gpt-4":
+      return getInputTokenLimit(model);
+    case "gpt-3.5-turbo":
+      return getOutputTokenLimit(model);
+  }
+}
+
+function getInputTokenLimit(model: Model): number | undefined {
+  if (model.name === "gpt-4") {
+    return model.inputTokenLimit;
+  }
+  return undefined;
+}
+
+function getOutputTokenLimit(model: Model): number | undefined {
+  if (model.name === "gpt-3.5-turbo") {
+    return model.outputTokenLimit;
+  }
+  return undefined;
+}
